8ddc3ca5fc82e362fff7131c6bfaee568d779449
mrloop <ewan@mrloop.com>
2021-05-13 22:52:30 +0100

    Combine files and download

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,21 +1,58 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { capitalize } from '@ember/string';
+import sportsLibPkg from '@sports-alliance/sports-lib';
+import utilitiesPkg from '@sports-alliance/sports-lib/lib/events/utilities/event.utilities.js';
+import exporterPkg from '@sports-alliance/sports-lib/lib/events/adapters/exporters/exporter.gpx.js';
+
+const { SportsLib } = sportsLibPkg;
+const { EventExporterGPX } = exporterPkg;
+const { EventUtilities } = utilitiesPkg;
 
 export default class ApplicationController extends Controller {
-  @action onDrop(event) {
+  @action async onDrop(event) {
     event.preventDefault();
     let { dataTransfer } = event;
-    if (dataTransfer.items) {
-      [...dataTransfer.items].forEach((item, i) => {
-        if (item.kind == 'file') {
-          let file = item.getAsFile();
-          this.logFile(file, i);
+    let files = this.dataTransferToFiles(dataTransfer);
+    files.forEach(this.logFile);
+    let events = await Promise.all(files.map((f) => this.fileToEvent(f)));
+    let evt = EventUtilities.mergeEvents(events);
+    let gpxString = await new EventExporterGPX().getAsString(evt);
+    let blob = new Blob([gpxString], { type: 'application/xml' });
+    let url = window.URL.createObjectURL(blob);
+    window.location.assign(url);
+  }
+
+  async fileToEvent(file) {
+    let fnName = `fileToEvent${capitalize(file.name.split('.').pop())}`;
+    return this[fnName] && this[fnName](file);
+  }
+
+  async fileToEventFit(file) {
+    let arrayBuffer = await file.arrayBuffer();
+    return SportsLib.importFromFit(arrayBuffer);
+  }
+
+  async fileToEventGpx(file) {
+    let text = await file.text();
+    return SportsLib.importFromGPX(text);
+  }
+
+  async fileToEventTcx(file) {
+    let text = await file.text();
+    const xml = new DOMParser().parseFromString(text, 'application/xml');
+    return SportsLib.importFromTCX(xml);
+  }
+
+  dataTransferToFiles({ items, files }) {
+    if (items) {
+      return [...items].map((item) => {
+        if (item.kind === 'file') {
+          return item.getAsFile();
         }
       });
     } else {
-      [...dataTransfer.files].forEach((file, i) => {
-        this.logFile(file, i);
-      });
+      return [...files];
     }
   }
 
