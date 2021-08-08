42a6d0dd872cba20d7c927842c641feb97ccfafb
mrloop <ewan@mrloop.com>
2021-08-08 18:27:43 +0100

    Create and deploy to netlify staging environment
    In https://github.com/mrloop/activity-merge/settings/actions set
    'Require approval for all outside collaborators' to stop PR which
    change the workflow to delete all netlify sites from running
    automatically

---
+++ staging.js
@@ -0,0 +1,132 @@
+const puppeteer = require('puppeteer');
+const execa = require('execa');
+
+let timeout = 5000;
+
+function debug(data, ...args) {
+  if (process.env.DEBUG) {
+    console.log(data, ...args);
+  }
+}
+
+async function siteName() {
+  let appName = 'activity-merge';
+  let { stdout: branchName } = await execa('git', ['branch', '--show-current']);
+  // https://stackoverflow.com/questions/58033366/how-to-get-current-branch-within-github-actions
+  // https://docs.github.com/en/actions/reference/environment-variables
+  branchName = branchName || process.env.GITHUB_HEAD_REF.split('/').pop();
+  return `${appName}-${branchName}`;
+}
+
+async function findSite() {
+  let { stdout } = await execa(
+    'yarn',
+    ['--silent', 'netlify', 'sites:list', '--json'],
+    { timeout }
+  );
+  let sites = JSON.parse(stdout);
+  let sitename = await siteName();
+  return sites.find(({ name }) => name === sitename);
+}
+
+async function deleteSite() {
+  let site = await findSite();
+  if (site) {
+    let { stdout } = await execa(
+      'yarn',
+      ['--silent', 'netlify', 'sites:delete', '--force', site.id],
+      { timeout }
+    );
+    debug(stdout);
+  }
+}
+
+async function findOrCreateSite() {
+  let accountName = 'mrloop';
+  let site = await findSite();
+  let sitename = await siteName();
+  if (site) {
+    debug('Site found:', sitename);
+  } else {
+    debug('Site not found:', sitename);
+    let { stdout: json } = await execa(
+      'yarn',
+      [
+        '--silent',
+        'netlify',
+        'sites:create',
+        '--json',
+        '--account-slug',
+        accountName,
+        '--name',
+        sitename,
+      ],
+      { timeout }
+    );
+    debug(json);
+    site = JSON.parse(json);
+    debug('Site created');
+  }
+  return site;
+}
+
+async function deploy() {
+  try {
+    let { id } = await findOrCreateSite();
+    let { stdout: revision } = await execa('git', [
+      'log',
+      '--pretty=%h',
+      '-n1',
+    ]);
+    let message = `Revision ${revision}`;
+    debug('Deploying', message);
+    let { stdout } = await execa(
+      'yarn',
+      [
+        '--silent',
+        'netlify',
+        'deploy',
+        '--dir=dist',
+        '--prod',
+        '--message',
+        message,
+        '--site',
+        id,
+        '--json',
+      ],
+      { timeout }
+    );
+    return stdout;
+  } catch (error) {
+    console.error(error);
+    /* eslint-disable no-process-exit */
+    process.exit(1);
+  }
+}
+
+async function checkDeploy(url, selector) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(selector, { timeout: 10000 });
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+    /* eslint-disable no-process-exit */
+    process.exit(1);
+  }
+}
+
+async function deployAndCheck() {
+  let json = await deploy();
+  let { url } = JSON.parse(json);
+  await checkDeploy(url, '.drop-zone');
+  console.log(json);
+}
+
+if (process.env.DELETE_SITE) {
+  deleteSite();
+} else {
+  deployAndCheck();
+}
